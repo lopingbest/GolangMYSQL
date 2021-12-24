9a7a2df9565f903de3adfa13917222b56f840b51
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-24 21:20:19 +0700

    feat:sql_test add TestQuerySql

--- sql_test.go
+++ sql_test.go
@@ -13,10 +13,25 @@ func TestExecSql(t *testing.T) {
 	ctx := context.Background()
 
 	script := "INSERT INTO customer(id, name) VALUES('joko','Joko')"
+	//exec tidak akan mengembalikan result
 	_, err := db.ExecContext(ctx, script)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
 }
