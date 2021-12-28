43edd81732877aec375846439eb8787c8b7e2f37
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-28 09:24:18 +0700

    feat:GolangMYSQL add nullable query and database parseTime

--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package Golang_MYSQL
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -48,5 +50,52 @@ func TestQuerySql(t *testing.T) {
 		//output
 		fmt.Println("Id:", id)
 		fmt.Println("Name:", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	//kalo data sudah tidak ada makan akan muncul panic
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	//selama masih ada data di rows, maka akan terus diambil
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		//data/timestamp tetep menggunakan time.Time kalau database cuma sampai tanggal, maka nanti jam menit detiknya akan kosong semua
+		var birthDate sql.NullTime
+		var createdAt time.Time
+		var married bool
+
+		//didalam kurung dimasukan parameterdari apa yang akan  kita ambil
+		//Pointer dipakai karena kita akan ngeset data dari parameter. Kalo enggak pointer, maka data tidak akan dipakai
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid {
+			fmt.Println("Email:", email.String)
+		}
+		fmt.Println("Balance:", balance)
+		fmt.Println("Rating:", rating)
+		if birthDate.Valid {
+			fmt.Println("Birth Date:", birthDate.Time)
+		}
+		fmt.Println("Married:", married)
+		fmt.Println("Created At:", createdAt)
 	}
 }
